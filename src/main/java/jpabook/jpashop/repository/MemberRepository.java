c795c17aa6f1a9139efe950ee58d43f6a8ccca5b
hyunju <hyunju1109@icomsys.co.kr>
2023-03-06 18:31:49 +0900

    회원 서비스 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,22 +1,17 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.PersistenceUnit;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
-
-    @PersistenceUnit
-    private EntityManagerFactory emf;
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
